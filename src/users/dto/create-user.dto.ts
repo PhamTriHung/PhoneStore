fcf47489b08f90859e6995d464de4fc2d16a5670
Kuro_neko <phamtrihung5363@gmail.com>
2024-08-22 20:02:05 +0700

    Feat: add validation for email and password

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,34 @@
+import {
+  IsEmail,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
 export default class CreateUserDto {
+  @IsEmail({}, { message: 'Invalid email address' })
   email: string;
+
+  @IsString()
+  @MinLength(8, {
+    message: 'Password is too short. It should be at least 8 characters long.',
+  })
+  @MaxLength(20, {
+    message:
+      'Password is too long. It should not be more than 20 characters long.',
+  })
+  @Matches(/(?=.*[a-z])/, {
+    message: 'Password must contain at least one lowercase letter.',
+  })
+  @Matches(/(?=.*[A-Z])/, {
+    message: 'Password must contain at least one uppercase letter.',
+  })
+  @Matches(/(?=.*\d)/, {
+    message: 'Password must contain at least one number.',
+  })
+  @Matches(/(?=.*[@$!%*?&#])/, {
+    message: 'Password must contain at least one special character.',
+  })
   password: string;
 }
