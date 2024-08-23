e8ffd389230f0d13ae5a94b10c7ea3843a1edf0d
Kuro_neko <phamtrihung5363@gmail.com>
2024-08-23 18:07:31 +0700

    Feat: validate when register

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Req,
+  UseGuards,
+  ValidationPipe,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import CreateUserDto from 'src/users/dto/create-user.dto';
 import { AuthGuard } from '@nestjs/passport';
@@ -10,7 +17,7 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('register')
-  register(@Body() createUserDto: CreateUserDto) {
+  register(@Body(ValidationPipe) createUserDto: CreateUserDto) {
     return this.authService.register(createUserDto);
   }
 
