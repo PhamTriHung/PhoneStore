9c73efe737f312b74f5d93fb2be1ea3b5296a5ca
Kuro_neko <phamtrihung5363@gmail.com>
2024-08-23 23:38:15 +0700

    Feat: validate when create update and delete cart item

--- src/carts/dto/add-to-cart.dto.ts
+++ src/carts/dto/add-to-cart.dto.ts
@@ -1,5 +1,13 @@
+import { IsInt, IsUUID, Min } from 'class-validator';
+
 export class AddToCartDto {
+  @IsUUID(4)
   userId: string;
+
+  @IsUUID(4)
   productId: string;
+
+  @IsInt()
+  @Min(1)
   quantity: number;
 }
--- src/carts/dto/delete-from-card.dto.ts
+++ src/carts/dto/delete-from-card.dto.ts
@@ -1,4 +1,9 @@
+import { IsUUID } from 'class-validator';
+
 export class DeleteFromCartDto {
+  @IsUUID(4)
   userId: string;
+
+  @IsUUID(4)
   productId: string;
 }
