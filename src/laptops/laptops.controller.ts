39723f5c4b7ca10a4b99b5a9177b7c2752bae911
Kuro_neko <phamtrihung5363@gmail.com>
2024-09-03 20:28:14 +0700

    Refactor: remove unused entity

--- src/laptops/laptops.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('laptops')
-export class LaptopsController {}
--- src/phones/phones.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PhonesService } from './phones.service';
-import { PhonesController } from './phones.controller';
-
-@Module({
-  providers: [PhonesService],
-  controllers: [PhonesController]
-})
-export class PhonesModule {}
