e26d62521e92b329cca9ba2043a3ef15ff0ce687
Kuro_neko <phamtrihung5363@gmail.com>
2024-09-05 11:48:49 +0700

    Fix(cart): create cart with user id

--- src/carts/carts.service.ts
+++ src/carts/carts.service.ts
@@ -1,17 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Cart } from './cart.entity';
 import { CreateCartDto } from './dto/create-cart.dto';
+import { User } from 'src/users/users.entity';
+import { NotFoundError } from 'rxjs';
 
 @Injectable()
 export class CartsService {
   constructor(
     @InjectRepository(Cart) private cartRepository: Repository<Cart>,
+    @InjectRepository(User) private usersRepository: Repository<User>,
   ) {}
 
-  createCart(createCartDto: CreateCartDto) {
-    const newCart = this.cartRepository.create(createCartDto);
+  async createCart(createCartDto: CreateCartDto) {
+    const { userId } = createCartDto;
+
+    const newCart = this.cartRepository.create();
+
+    if (userId) {
+      const user = await this.usersRepository.findOneBy({ id: userId });
+
+      if (!user) {
+        throw new NotFoundException(`User with id ${userId} not found`);
+      } else {
+        newCart.user = user;
+      }
+    }
 
     return this.cartRepository.save(newCart);
   }
